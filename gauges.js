435cae720e0b4917104acaba67522077c6652968
kelvinperrie <kelvinperrie@gmail.com>
2020-06-11 14:01:04 +1200

    letting the pointer position be updated at a later time

--- gauges.js
+++ gauges.js
@@ -4,26 +4,10 @@ function getRandomIntForGauge(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function TwitchIt(element, settings) {
-    if(!settings.currentAngle) {
-        settings.currentAngle = settings.targetAngle;
-    }
-    var twitchTo = getRandomIntForGauge(settings.twitchMin, settings.twitchMax);
-    // if twitchTo is the same as current then do nothing for a while
-    if(twitchTo === settings.currentAngle) {
-        setTimeout(function() { TwitchIt(element, settings); } , settings.twitchDuration);
-    } else {
-        $(element).children(".pointer").rotate({
-            duration: settings.twitchDuration,
-            angle: settings.currentAngle,
-            animateTo: twitchTo,
-            callback: function() { settings.currentAngle = twitchTo; TwitchIt(element, settings); },
-            easing: function (x,t,b,c,d){ return c*(t/d)+b; }
-        });
-    }
-}
 
-$.fn.gaugeMe = function(settings) {
+
+var gaugeModel = function (element, settings) {
+    var self = this;
     var pointerImage, backingImage, pointerVerticalOffset, startAngle;
     switch (settings.gaugeType) {
         case 2:
@@ -43,37 +27,67 @@ $.fn.gaugeMe = function(settings) {
             backingImage = "Backing1";
             pointerVerticalOffset = '65px';
             startAngle = settings.startAngle || -90;
-        }
+    }
     // if set explicitly then take precedence
     // great consistency and use of extend, well done me
-    if(settings.pointerImage) {
+    if (settings.pointerImage) {
         pointerImage = settings.pointerImage;
     }
-    if(settings.backingImage) {
+    if (settings.backingImage) {
         backingImage = settings.backingImage;
     }
-    if(settings.pointerVerticalOffset) {
+    if (settings.pointerVerticalOffset) {
         pointerVerticalOffset = settings.pointerVerticalOffset;
     }
-    if(!settings.initialAnimationDuration) {
+    if (!settings.initialAnimationDuration) {
         settings.initialAnimationDuration = 2000;
     }
-    if(!settings.twitchDuration) {
+    if (!settings.twitchDuration) {
         settings.twitchDuration = 200;
     }
-    
-    var gauge = $(this);
-    $(this).addClass('gauge');
+
+    self.settings = settings;
+    var gauge = element;
+    element.addClass('gauge');
     // add the images we need; a background and a pointer
-    $(this).prepend($('<img>',{class:'pointer',src:'images/'+pointerImage+'.png', style:'top:'+pointerVerticalOffset+';', alt:'gauge pointer'}))
-    $(this).prepend($('<img>',{class:'backing',src:'images/'+backingImage+'.png', alt:'gauge background'}))
+    element.prepend($('<img>', { class: 'pointer', src: 'images/' + pointerImage + '.png', style: 'top:' + pointerVerticalOffset + ';', alt: 'gauge pointer' }));
+    element.prepend($('<img>', { class: 'backing', src: 'images/' + backingImage + '.png', alt: 'gauge background' }));
     // animate the pointer from 0 to the target angle, then start it twitching
-    $(this).children(".pointer").rotate({
+    element.children(".pointer").rotate({
         duration: settings.initialAnimationDuration,
         angle: startAngle,
         animateTo: settings.targetAngle,
-        callback: function() { 
-            TwitchIt(gauge, settings); 
+        callback: function () {
+            self.TwitchIt(gauge);
         }
     });
+
+    self.UpdateTargetAngle = function (targetAngle, twitchMin, twitchMax) {
+        self.settings.targetAngle = targetAngle;
+        self.settings.twitchMin = twitchMin;
+        self.settings.twitchMax = twitchMax;
+    };
+
+    self.TwitchIt = function (element) {
+        if (!self.settings.currentAngle) {
+            self.settings.currentAngle = settings.targetAngle;
+        }
+        var twitchTo = getRandomIntForGauge(settings.twitchMin, settings.twitchMax);
+        // if twitchTo is the same as current then do nothing for a while
+        if (twitchTo === self.settings.currentAngle) {
+            setTimeout(function () { self.TwitchIt(element, self.settings); }, self.settings.twitchDuration);
+        } else {
+            $(element).children(".pointer").rotate({
+                duration: self.settings.twitchDuration,
+                angle: self.settings.currentAngle,
+                animateTo: twitchTo,
+                callback: function () { self.settings.currentAngle = twitchTo; self.TwitchIt(element); },
+                easing: function (x, t, b, c, d) { return c * (t / d) + b; }
+            });
+        }
+    };
+};
+
+$.fn.gaugeMe = function(settings) {
+    return new gaugeModel($(this), settings);
 };
